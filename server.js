4c1f1136c82c75078193241025c28142c590b3b1
steve28818183232-glitch <steve28818183232@gmail.com>
2026-06-10 13:26:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,36 +6,48 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// 讓伺服器讀取網頁檔案
 app.use(express.static(__dirname));
 
-// 紀錄所有在線上的玩家
 const players = {};
+// 給玩家隨機分配可愛的瑪利歐風格角色
+const avatars = ['🍄', '🐢', '🌟', '👻', '🐶', '🐱'];
+const boardLength = 14; // 我們的環狀地圖總共有 14 格
 
 io.on('connection', (socket) => {
     console.log('有新玩家加入：' + socket.id);
     
-    // 給新玩家一個隨機顏色和初始位置
+    // 玩家初始狀態設定
     players[socket.id] = {
-        x: 300,
-        y: 200,
-        color: '#' + Math.floor(Math.random()*16777215).toString(16)
+        id: socket.id,
+        position: 0, // 大家都在第 0 格 (起點) 出生
+        coins: 0,    // 初始金幣為 0
+        avatar: avatars[Math.floor(Math.random() * avatars.length)]
     };
     
-    // 把目前所有人傳給新玩家
     socket.emit('currentPlayers', players);
-    // 通知其他人有新玩家來了
-    socket.broadcast.emit('newPlayer', { id: socket.id, player: players[socket.id] });
+    socket.broadcast.emit('newPlayer', players[socket.id]);
 
-    // 接收玩家移動的指令
-    socket.on('playerMovement', (movementData) => {
-        players[socket.id].x = movementData.x;
-        players[socket.id].y = movementData.y;
-        // 廣播給所有人更新畫面
-        io.emit('playerMoved', { id: socket.id, player: players[socket.id] });
+    // 接收玩家按鈕「擲骰子」的指令
+    socket.on('rollDice', () => {
+        const player = players[socket.id];
+        if (!player) return;
+        
+        // 隨機產生 1 到 6 的步數
+        const steps = Math.floor(Math.random() * 6) + 1;
+        let newPosition = player.position + steps;
+        
+        // 【核心邏輯】經過或回到終點 (第 0 格)，給予 Bonus 獎勵！
+        if (newPosition >= boardLength) {
+            player.coins += 10; // 領取 10 個金幣的 Bonus！
+            newPosition = newPosition % boardLength; // 讓位置重新繞回前面
+        }
+        
+        player.position = newPosition;
+        
+        // 廣播擲骰子結果與更新後的玩家狀態給所有人
+        io.emit('diceResult', { id: socket.id, steps: steps, player: player });
     });
 
-    // 玩家離開
     socket.on('disconnect', () => {
         console.log('玩家離開：' + socket.id);
         delete players[socket.id];
